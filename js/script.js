b5cc56ee4aec179ab27091a9a164108950a0af86
CarlosGGarro <carlosggarroest.gmail.com>
2025-05-13 02:15:16 -0300

    Hice un Carrousel con Cards funciona con su CSS y JS, falta hacer que se adapte bien en pantallas chicas

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".slide-content", {
+      slidesPerView: 3,
+      spaceBetween: 55,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
