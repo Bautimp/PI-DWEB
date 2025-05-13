0569945b081dde189c2197bf1bc4f5c71228f0f7
Bautimp <bautistamontesp@gmail.com>
2025-05-13 17:30:33 -0300

    arreglo de unos errores

--- js/script.js
+++ js/script.js
@@ -1,19 +1,8 @@
 var swiper = new Swiper(".slide-content", {
-      slidesPerView: 2,
+      slidesPerView: 3,
       spaceBetween: 55,
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
       },
-      breakpoints: {
-      0: {
-        spaceBetween: 100
-      },
-      700: {
-        spaceBetween: 70
-      },
-      900: {
-        spaceBetween: 55
-      }
-    }
     });
