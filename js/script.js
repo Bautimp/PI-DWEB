f1d62b5ceff2bdead28344fbd7e913b6a4f033a5
Bautimp <bautistamontesp@gmail.com>
2025-06-13 00:55:06 -0300

    Armando funció nde js para muestra según la imágen del carousel seleccionada

--- js/script.js
+++ js/script.js
@@ -13,3 +13,32 @@ const cargarArchivo = () => {
 
 
 }
+
+function mostrarInfo() {
+    let descripcionService=document.getElementById('descripcionService');
+    let serviceSeleccionado=document.getElementById('carouselServices');
+    let arregloImagenes = [document.getElementById("img1"), document.getElementById("img2"), document.getElementById("img3"), document.getElementById("img4"), document.getElementById("img5")];
+
+    for (let i=0; i < arregloImagenes.length; i++) {
+        console.log(arregloImagenes[i].className);
+        if((arregloImagenes[i].className).includes("active")) {
+            
+            console.log(arregloImagenes[i].value);
+
+            switch(arregloImagenes[i].value){
+                case 1:
+                    descripcionService.innerHTML="Este es el service de rutina";
+                    break;
+                case 2:
+                    descripcionService.innerHTML="Este el service de estandar";
+            }
+        }
+    }
+    /*
+
+    console.log(serviceSeleccionado);
+    switch(serviceSeleccionado){
+
+    }
+    */
+}
